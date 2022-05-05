e853ee2068ca0c5e2dfb99d0e06fb08dd640d7de
Amit Kumar Yadav <96667638+amit-2526@users.noreply.github.com>
2022-05-05 22:51:51 +0530

    Add files via upload

---
+++ binarytree1.java
@@ -0,0 +1,13 @@
+public class binarytree1 {
+
+    class node {
+        int key;
+        node left , right;
+
+        public node(int item){
+            key = item;
+            left = right = null;
+        }
+    }
+    
+}
